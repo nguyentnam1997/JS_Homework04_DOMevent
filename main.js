bb97fc9f98ad9641af5ce73b0df65922a9b060a3
nguyentnam1997 <nguyentnam.061297@gmail.com>
2023-11-28 15:34:45 +0700

    123

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
 let colors = ["#3498db", "#9b59b6", "#e74c3c", "#2c3e50", "#d35400"];
-const boxes = document.querySelector(".boxes");
-console.log(boxes);
+var boxes = document.querySelector(".boxes");
 
-//Function for click the button "More boxes"
-const addBox = function (colors) {
+//Total box: 5
+var totalBox = document.querySelector(".points");
+totalBox.innerHTML = 5;
+//Function click the box to remmove it
+var removeBox = function (box) {
+  box.addEventListener("click", function () {
+    box.remove();
+    var boxesCount = document.querySelectorAll(".box");
+    totalBox.innerHTML = boxesCount.length;
+  });
+};
+
+//Function generate box
+var addBox = function (colors) {
   colors.forEach((color) => {
-    const box = document.createElement("div");
+    var box = document.createElement("div");
     box.classList.add("box");
     box.style.backgroundColor = color;
     boxes.appendChild(box);
+    removeBox(box);
   });
 };
 addBox(colors);
 
-//Generate number of boxes
-const boxesCount = document.querySelectorAll('.box');
-console.log(boxesCount);
-const totalBox = document.querySelector(".points");
-totalBox.innerHTML = boxesCount.length;
-
-//Click the box to delete it
-boxes.addEventListener("click", function () {
-    if (box.classList.contains("box")) {
-        box.remove();
-        totalBox.innerHTML = "";
-        totalBox.innerHTML = boxesCount.length;
-    }
+var btn = document.getElementById("btn");
+console.log(btn);
+btn.addEventListener("click", function () {
+  addBox(colors);
+  var boxesCount = document.querySelectorAll(".box");
+  totalBox.innerHTML = boxesCount.length;
 });
